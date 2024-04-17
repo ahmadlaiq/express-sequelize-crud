36eae8103922c7d54e4b253e6215005e9103e74d
Ahmad Nurul Laiq <ahmadlaiq97@gmail.com>
2024-04-17 18:50:11 +0700

    chore: morgan, sebuah middleware untuk ExpressJS, untuk memantau permintaan HTTP.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
 // Importing routes
 const CategoriesRouter = require('./routes/catagories');
+const morgan = require('morgan')
 
 // Middleware
 app.use(express.json());
+
+// Morgan
+app.use(morgan('dev'));
 
 // Routing
 app.use('/api/v1/categories', CategoriesRouter);
