857ada9e3f3e586a8fa3c83dd43e13c3efcb5922
Ahmad Nurul Laiq <ahmadlaiq97@gmail.com>
2024-04-18 09:06:39 +0700

    chore: setup sequelize-cli

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-// Importing routes
+const dotenv = require('dotenv');
+const cors = require('cors');
 const CategoriesRouter = require('./routes/catagories');
-const morgan = require('morgan')
+const morgan = require('morgan');
+
+dotenv.config();
 
 // Middleware
-app.use(express.json());
-
-// Morgan
-app.use(morgan('dev'));
+app.use(express.json()); // Parse JSON bodies
+app.use(morgan('dev')); // Logging middleware
+app.use(cors()); // Enable CORS
 
 // Routing
 app.use('/api/v1/categories', CategoriesRouter);
 
-// Start server
+// Server
+const port = process.env.PORT || 3000; // Use port from environment variable or default to 3000
 app.listen(port, () => {
     console.log(`Server berjalan di port ${port}`);
 });
